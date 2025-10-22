7dc3de13e0beb64b672311ce7f4129705192ad69
Quân <117747610+dnghngqun@users.noreply.github.com>
2025-10-22 21:01:06 +0700

    up prj

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { CartProvider } from "@/components/cart-provider";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { Toaster } from "@/components/ui/toaster";
+import { Inter } from "next/font/google";
+import type React from "react";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "PetPals - What your pet needs, when they need it",
+  description: "Premium pet products for cats and dogs with same-day delivery",
+  generator: "v0.dev",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <CartProvider>
+          <div className="flex min-h-screen flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+          <Toaster />
+        </CartProvider>
+      </body>
+    </html>
+  );
+}
